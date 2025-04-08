8f0a62d8dbb28151cc16f54a37cc6ef5b2bd51b8
Eldrige <peldrige8@gmail.com>
2025-04-08 08:48:59 +0200

    rm middleware

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,8 @@
 import express from 'express';
 import { authWithGoogle } from '../controllers/user-controller.js';
-import { authenticateUser as authenticateUserMiddleware } from '../middleware/auth-middleware.js';
 
 const router = express.Router();
 
 router.post('/google-login', authWithGoogle);
-
-router.use(authenticateUserMiddleware);
 
 export default router;
